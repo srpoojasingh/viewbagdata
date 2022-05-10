ec3e825598c0bff5aa168fca316f4cc71f30b514
Pooja Singh <poojasinghsr9876@gmail.com>
2022-05-10 21:03:11 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewBagD.Models;
+
+namespace ViewBagD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This is Pooja";
+            ViewBag.List = new List<string>
+            {
+                "Pooja",
+                "Ravi"
+            };
+            string[] str = { "p", "o", "o", "j", "a" };
+            ViewBag.Array= str;
+            Employee emp = new Employee();
+            emp.EmpId = 1;
+            emp.EmpName = "Pooja";
+            emp.EmpSal = 30000;
+            ViewBag.Employee = emp;
+            ViewBag.CommonMessage = "Achieved By ViewData";
+            ViewData["Common"] = "Achieved Common By ViewBag";
+
+            return View();
+        }
+
+       
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewBagD.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public float EmpSal { get; set; }
+    }
+}
